b99da15ae9ad8d05d16897ef062f5f4f9f163a16
Lucas Felix <Lucas@glide.com.br>
2024-07-25 17:39:57 -0300

    feat(navBar): add menu categories component

---
+++ src/components/layout/navBar/MenuCategories.jsx
@@ -0,0 +1,17 @@
+import styles from './NavBar.module.css'
+const MenuCategories = () => {
+  return (
+    <div className="bg-white mt-4 rounded">
+        <ul className={`${styles.menuCategories} w-[350px] h-[125px] flex flex-col justify-evenly`}> 
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+  )
+}
+
+export default MenuCategories
+
+
+
--- src/components/layout/navBar/NavBar.jsx
+++ src/components/layout/navBar/NavBar.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import styles from "./NavBar.module.css";
 import MenuButton from "./MenuButton";
 import MenuIcons from "./MenuIcons";
+import MenuCategories from "./MenuCategories";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <nav
-    className={`${styles.navBar} bg-slate-900 w-[1220px] h-[60px] ml-auto mr-auto flex justify-between items-center`}
-    >
-      <MenuButton />
-      <h1 className="text-center text-white">HardStore</h1>
-      <MenuIcons />
-    </nav>
+    <>
+      <nav
+        className={`${styles.navBar} bg-slate-900 ml-auto mr-auto flex justify-between items-center`}
+      >
+        <div onClick={handleMenuClick}>
+          <MenuButton />
+        </div>
+        <h1 className="text-center text-white">HardStore</h1>
+        <MenuIcons />
+      </nav>
+      {showMenu && (
+        <MenuCategories />
+      )}
+    </>
   );
 };
 export default NavBar;
