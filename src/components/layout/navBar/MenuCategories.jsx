35d5dafb7206cb6bcd44ab41ec9bee671f5f3cd8
Lucas Felix <Lucas@glide.com.br>
2024-08-15 14:37:03 -0300

    style(menus): change to absolute positioning

--- src/components/layout/navBar/MenuCategories.jsx
+++ src/components/layout/navBar/MenuCategories.jsx
@@ -3,8 +3,8 @@ import { SlArrowRight } from "react-icons/sl";
 
 const MenuCategories = () => {
   return (
-    <div className="bg-white mt-4 rounded">
-        <ul className={`${styles.menuCategories} absolute left-[5%] min-w-56 z-10 min-h-32 flex flex-col justify-evenly p-2`}> 
+    <div className="bg-white mt-2 rounded absolute left-[5%]">
+        <ul className={`${styles.menuCategories} min-w-56 z-10 min-h-32 flex flex-col justify-evenly p-2`}> 
             <li><p>Processors</p><SlArrowRight/></li>
             <li><p>Mother Boards</p><SlArrowRight/></li>
             <li><p>Graphic Cards</p><SlArrowRight/></li>
--- src/components/layout/navBar/MenuSearch.jsx
+++ src/components/layout/navBar/MenuSearch.jsx
@@ -1,8 +1,8 @@
 import styles from './NavBar.module.css';
 const MenuSearch = () => {
   return (
-    <div className="bg-white mt-4 rounded">
-        <div className={`${styles.menuCategories} absolute right-[5%] z-10`}> 
+    <div className="bg-white mt-2 rounded absolute right-[5%] z-10">
+        <div className={`${styles.menuCategories}`}> 
           <input type="search" placeholder='Search...' className='p-2 outline-none' />
         </div>
     </div>
