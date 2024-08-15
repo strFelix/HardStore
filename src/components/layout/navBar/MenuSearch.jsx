8b2e66c7b503336f941663fd4338f51b0e6c28b9
Lucas Felix <Lucas@glide.com.br>
2024-08-15 13:34:43 -0300

    feat(menuSearch): add search input

--- src/components/layout/navBar/MenuSearch.jsx
+++ src/components/layout/navBar/MenuSearch.jsx
@@ -1,7 +1,10 @@
+import styles from './NavBar.module.css';
 const MenuSearch = () => {
   return (
-    <div>
-      MenuSearch
+    <div className="bg-white mt-4 rounded">
+        <div className={`${styles.menuCategories} absolute right-[5%] z-10`}> 
+          <input type="search" placeholder='Search...' className='p-2 outline-none' />
+        </div>
     </div>
   );
 };
