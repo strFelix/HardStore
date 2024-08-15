eeeb9c2755c56b61a3532505bfa64749bd783bc0
Lucas Felix <Lucas@glide.com.br>
2024-08-15 09:13:51 -0300

    feat(menuCategories): add styles and arrow icon

--- src/components/layout/navBar/MenuCategories.jsx
+++ src/components/layout/navBar/MenuCategories.jsx
@@ -1,11 +1,13 @@
 import styles from './NavBar.module.css'
+import { SlArrowRight } from "react-icons/sl";
+
 const MenuCategories = () => {
   return (
     <div className="bg-white mt-4 rounded">
-        <ul className={`${styles.menuCategories} absolute left-[5%] min-w-96 z-10 min-h-32 flex flex-col justify-evenly`}> 
-            <li></li>
-            <li></li>
-            <li></li>
+        <ul className={`${styles.menuCategories} absolute left-[5%] min-w-56 z-10 min-h-32 flex flex-col justify-evenly p-2`}> 
+            <li><p>Processors</p><SlArrowRight/></li>
+            <li><p>Mother Boards</p><SlArrowRight/></li>
+            <li><p>Graphic Cards</p><SlArrowRight/></li>
         </ul>
     </div>
   )
